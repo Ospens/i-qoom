c6c23df7845ba7e291921e830654b168b94b4de4
Kirill Shamardin <shamardin.k@gmail.com>
2019-03-11 22:01:55 +0700

    simple users registration was added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      render json: { status: "success",
+                     message: t(".success_message") },
+             status: :ok  
+    else
+      render json: { status: "error",
+                     message: t(".error_message"),
+                     error_messages: @user.errors.full_messages,
+                     fields_with_errors: @user.errors.messages.keys },
+             status: :ok
+    end
   end
 
   def update
@@ -10,6 +21,12 @@ class Api::V1::UsersController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
