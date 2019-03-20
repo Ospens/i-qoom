72d920fb805c953b949cfd411770b8a3ed04d17c
Kirill Shamardin <shamardin.k@gmail.com>
2019-03-20 15:22:54 +0700

    sessions destroy spec was removed

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -3,6 +3,4 @@ require 'rails_helper'
 describe Api::V1::ContactsController, type: :routing do
   it{ expect(post("/api/v1/sessions")).to\
       route_to("api/v1/sessions#create") }
-  it{ expect(delete("/api/v1/sessions/1")).to\
-      route_to("api/v1/sessions#destroy", id: "1") }
 end
