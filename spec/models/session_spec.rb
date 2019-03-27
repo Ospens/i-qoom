dad5aa90fbd36013bbba873af45539c36cdc6d0f
Kirill Shamardin <shamardin.k@gmail.com>
2019-03-27 14:25:06 +0700

    requests tests were added for user and session

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
 describe Session, type: :model do
-
+  user = FactoryBot.create(:user)
   context "is expected to be successfull" do
-    user = FactoryBot.create(:user)
-    it "with email" do
+    it "with an email" do
       session = Session.new(login: user.email, password: "password1")
       expect(session.auth_token).to be_truthy
     end
-    it "with username" do
+    it "with a username" do
       session = Session.new(login: user.username, password: "password1")
       expect(session.auth_token).to be_truthy
     end
   end
 
   context "is expected not to be successfull" do
-    user = FactoryBot.create(:user)
     context "without" do 
       it "login" do
         session = Session.new(password: "password1")
---
+++ spec/requests/session_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Session", type: :request do
+  headers = { "CONTENT_TYPE" => "application/json" }
+  user = FactoryBot.create(:user)
+
+  context "sign in" do
+    it 'should get a status "error"' do
+      post "/api/v1/sessions",
+        params: { session: { login: [ "z" + user.email,
+                                      user.username + "1" ].sample,
+                             password: "password1" } }.to_json,
+        headers: headers
+      expect(response).to have_http_status(:success)
+      expect(JSON(response.body)["status"]).to eq("error")
+    end
+
+    it 'should get a status "success"' do
+      post "/api/v1/sessions",
+        params: { session: { login: [ user.email,
+                                      user.username ].sample,
+                             password: "password1" } }.to_json,
+        headers: headers
+      expect(response).to have_http_status(:success)
+      expect(JSON(response.body)["status"]).to eq("success")
+    end
+  end
+end
