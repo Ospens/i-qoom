be2b6bc52ae92de4142005d9b7eb72fbe95e70f0
Unknown <1ospens1@gmail.com>
2019-04-11 18:02:04 +0300

    State and City fields

--- app/javascript/components/landing/LandingPageHeader.jsx
+++ app/javascript/components/landing/LandingPageHeader.jsx
@@ -35,6 +35,7 @@ class LandingPageHeader extends Component {
             <div className='collapse navbar-collapse offset'>
               <ul className={`nav navbar-nav menu_nav justify-content-center ${navClass}`}>
                 <li className='nav-item'><a href='#' className='nav-link'>Start</a></li>
+                <li className='nav-item'><a href='#samples-card' className='nav-link'>Examples</a></li>
                 <li className='nav-item'><a href='#what-is-card' className='nav-link'>i-Qoom</a></li>
                 <li className='nav-item'><a href='#pricing-card' className='nav-link'>Pricing</a></li>
                 <li className='nav-item'><a href='#reviews-card' className='nav-link'>Reviews</a></li>
