8c60a3f8ee56f82e979e19fd223739fc123bf393
Unknown <1ospens1@gmail.com>
2019-04-12 08:10:49 +0300

    Project overview

--- app/javascript/components/dashboard/Dashboard.jsx
+++ app/javascript/components/dashboard/Dashboard.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import axios from 'axios';
+import axios from 'axios'
+import ReactSVG from 'react-svg'
+import './Dashboard.scss'
+import logo from '../../images/Logo_header'
+import plus from '../../images/add_1'
+import bell from '../../images/alarm-bell'
+import messages from '../../images/email-action-unread'
+import tmpAvatar from '../../images/colors'
 
 class Dashboard extends Component {
 
   state = {
-    user: null
+    user: null,
+    showModal: false
   }
 
   componentDidMount() {
@@ -18,12 +26,148 @@ class Dashboard extends Component {
       .then(res => this.setState({user: res.data.hello}))
   }
 
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
+  }
+
+  handleClickOutside = (event) => {
+    if (this.state.showModal && this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      alert('You clicked outside of me!');
+    }
+  }
+
   render() {
-    const { user } = this.state
+    const { user, showModal } = this.state
     return (
-      <h1>
-        Hello, {user}
-      </h1>
+      <div className='container-fluid'>
+        <div className='row'>
+          <nav className='col-md-2 d-none d-md-block sidebar'>
+            <div className='sidebar-sticky'>
+              <a className='navbar-brand logo_h' href='#'>
+                <ReactSVG
+                  svgStyle={{ height: 30, width: 100 }}
+                  src={logo}
+                />
+              </a>
+              <ul className='nav flex-column'>
+                <li className='nav-item'>
+                  <a className='nav-link active'>Project ovreview</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div className='col-md-9 ml-sm-auto col-lg-10 px-5'>            
+            <div className='user-info'>
+              <ul>
+                <li className='nav-item'>
+                  <button type='button' className='nav-link btn-transparent'>
+                    <ReactSVG
+                      svgStyle={{ height: 15, marginRight: 10 }}
+                      src={messages}
+                    />
+                  </button>
+                </li>
+                <li className='nav-item'>
+                  <button type='button' className='nav-link btn-transparent'>
+                    <ReactSVG
+                      svgStyle={{ height: 20, marginRight: 10 }}
+                      src={bell}
+                    />
+                  </button>
+                </li>
+                <li className='nav-item'>
+                  <button type='button' className='nav-link btn-transparent'>
+                    <ReactSVG
+                      svgStyle={{ height: 30, width: 30 }}
+                      src={tmpAvatar}
+                    />
+                  </button>
+                </li>
+              </ul>
+            </div>
+
+            <main role='main' className='dashboard-content'>
+              <h2 className='page-title'>Project ovreview</h2>
+              
+              <div className='row row-projects'>
+                <div className='col-sm-4'>
+                  <div className='project-card'>
+                    <ReactSVG
+                      svgStyle={{ height: 20, width: 20 }}
+                      src={plus}
+                    />
+                    <label>Create a new project</label>
+                  </div>
+                </div>
+                <div className='col-sm-4'>
+                  <div className='project-card'>
+                    <ReactSVG
+                      svgStyle={{ height: 20, width: 20 }}
+                      src={plus}
+                    />
+                    <label>Create a new project</label>
+                  </div>
+                </div>
+                <div className='col-sm-4'>
+                  <div className='project-card'>
+                    <ReactSVG
+                      svgStyle={{ height: 20, width: 20 }}
+                      src={plus}
+                    />
+                    <label>Create a new project</label>
+                  </div>
+                </div>
+              </div>
+              <div className='statuses'>
+                <label>Status</label>
+                <ul>
+                  <li>
+                    <span className='green-dot'/>
+                    <span>Active</span>
+                  </li>
+                  <li>
+                    <span className='yellow-dot' />
+                    <span>In Preparation</span>
+                  </li>
+                </ul>
+              </div>
+
+              <button type='button' className='btn btn-primary' onClick={this.handleOpenModal}>
+                Launch demo modal
+              </button>
+
+              <div
+                className={`modal fade ${showModal ? 'show' : ''}`}
+                id='exampleModalLong'
+                tabIndex='-1'
+                role='dialog'
+                aria-modal='true'
+              >
+                <div className='modal-dialog' role='document'>
+                  <div className='modal-content'>
+                    <div className='modal-body'>
+                      <h4>Welcome to i-Qoom!</h4>
+                      <p>You are now in the Project overview. This is where you will create and manage your projects.</p>
+                      <p>So, let's start your first project.</p>
+                    </div>
+                    <div className='modal-footer'>
+                      <button type='button' className='btn btn-secondary' data-dismiss='modal' onClick={this.handleCloseModal}>Cancel</button>
+                      <button type='button' className='btn btn-primary'>Let's go</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {showModal && <div className='modal-backdrop fade show'></div>}
+            </main>
+          </div>
+        </div>
+      </div>
     )
   }
 }
