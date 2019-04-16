9257b3e039d0c68b62ce4924ecf3a2a56380e0dc
Kirill Shamardin <shamardin.k@gmail.com>
2019-04-16 14:36:34 +0700

    project administrator spec was updated

--- spec/models/project_administrator_spec.rb
+++ spec/models/project_administrator_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ProjectAdministrator, type: :model do
+  it { is_expected.to belong_to(:project) }
+  it { is_expected.not_to validate_presence_of(:user) }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to allow_value(Faker::Internet.email).for(:email) }
 end
