8a3f9afb9cb2262daf8a42a64c05f2b4c96015dc
Unknown <1ospens1@gmail.com>
2019-04-25 08:10:43 +0300

    Toaster

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import mainStore from '../stores/mainStore'
+import { ToastContainer } from 'react-toastify'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../styles/App.scss'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={mainStore} >
       <App />
+      <ToastContainer />
     </Provider>,
     document.getElementById('app'),
   )
