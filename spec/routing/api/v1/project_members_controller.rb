01982012816e5903c4e6103928b9e1267be00ca8
Kirill Shamardin <shamardin.k@gmail.com>
2019-06-26 14:50:35 +0700

    routing tests were added for project members

---
+++ spec/routing/api/v1/project_members_controller.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::ProjectMembersController, type: :routing do
+  describe "routing" do
+    it "routes to #show" do
+      expect(get: "/api/v1/projects/1/members/1").to\
+        route_to("api/v1/project_members#show",
+                 project_id: "1",
+                 id: "1")
+    end
+    it "routes to #index" do
+      expect(get: "/api/v1/projects/1/members").to\
+        route_to("api/v1/project_members#index",
+                 project_id: "1")
+    end
+
+    it "routes to #new" do
+      expect(get: "/api/v1/projects/1/members/new").to\
+        route_to("api/v1/project_members#new",
+                 project_id: "1")
+    end
+
+    it "routes to #create" do
+      expect(post: "/api/v1/projects/1/members").to\
+        route_to("api/v1/project_members#create",
+                 project_id: "1")
+    end
+
+    it "routes to #edit" do
+      expect(get: "/api/v1/projects/1/members/1/edit").to\
+        route_to("api/v1/project_members#edit",
+                 project_id: "1",
+                 id: "1")
+    end
+
+    it "routes to #update" do
+      expect(patch: "/api/v1/projects/1/members/1").to\
+        route_to("api/v1/project_members#update",
+                 project_id: "1",
+                 id: "1")
+    end
+
+    it "routes to #update" do
+      expect(put: "/api/v1/projects/1/members/1").to\
+        route_to("api/v1/project_members#update",
+                 project_id: "1",
+                 id: "1")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/api/v1/projects/1/members/1").to\
+        route_to("api/v1/project_members#destroy",
+                 project_id: "1",
+                 id: "1")
+    end
+  end
+end
