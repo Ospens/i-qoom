4603c1d80926435058662a7122e2b6f71fe17c12
Kirill Shamardin <shamardin.k@gmail.com>
2019-08-09 14:55:31 +0700

    first and last name project member min length validation was changed from 3 to 2

--- spec/models/project_member_spec.rb
+++ spec/models/project_member_spec.rb
@@ -58,12 +58,12 @@ RSpec.describe ProjectMember, type: :model do
                             .for(:email) }
       it { is_expected.to validate_presence_of(:first_name) }
       it { is_expected.to validate_length_of(:first_name)
-                                .is_at_least(3)
+                                .is_at_least(2)
                                 .is_at_most(255) }
 
       it { is_expected.to validate_presence_of(:last_name) }
       it { is_expected.to validate_length_of(:last_name)
-                                .is_at_least(3)
+                                .is_at_least(2)
                                 .is_at_most(255) }
     end
   end
