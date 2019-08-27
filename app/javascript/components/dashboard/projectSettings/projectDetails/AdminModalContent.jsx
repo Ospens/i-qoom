8381baf8a844969410696386274a70e10c3c9d91
Ilya <1ospens1@gmail.com>
2019-08-27 09:07:46 +0300

    Project settings fixes

--- app/javascript/components/dashboard/projectSettings/projectDetails/AdminModalContent.jsx
+++ app/javascript/components/dashboard/projectSettings/projectDetails/AdminModalContent.jsx
@@ -12,22 +12,19 @@ function AdminModalContent({ handleSubmit, close, projectId }) {
     [dispatch])
 
   return (
-    <form
-      className='new-project-modal'
-      onSubmit={handleSubmit(updateAdmin)}
-    >
-      <h4>New project administrator</h4>
-      <div className='modal-body'>
+    <form className='new-modal' onSubmit={handleSubmit(updateAdmin)}>
+      <div className='new-modal__header'><h4>New project administrator</h4></div>
+      <div className='new-modal__body'>
         <div>
-          <h6>Who is the new project administrator?</h6>
+          <h6 className='new-modal__body-title'>Who is the new project administrator?</h6>
           <label className='project-admin'>Another project administrator</label>
           <AdministratorFields />
         </div>
       </div>
-      {<div className='modal-footer'>
+      {<div className='new-modal__footer'>
         <button
           type='button'
-          className='btn btn-white'
+          className='btn btn-white mr-2'
           onClick={close}
         >
           Cancel
