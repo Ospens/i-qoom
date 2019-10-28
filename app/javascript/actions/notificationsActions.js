65c469b636dd924dfbae550c9cd3d8f78b2b0639
Ospens <1ospens1@gmail.com>
2019-10-28 17:30:02 +0300

    Notifications

--- app/javascript/actions/notificationsActions.js
+++ app/javascript/actions/notificationsActions.js
@@ -4,20 +4,20 @@ import {
   REMOVE_NOTIFICATION
 } from './types'
 
-let timer
+let offTimer
 
 export const stopStateOffTimer = () => {
-  clearTimeout(timer)
-  timer = undefined
+  clearTimeout(offTimer)
+  offTimer = undefined
 }
 
 export const toggleState = payload => dispatch => dispatch(({ type: TOGGLE_STATE, payload }))
 
 const stateOffTimer = dispatch => {
-  if (timer) {
-    clearTimeout(timer)
+  if (offTimer) {
+    clearTimeout(offTimer)
   }
-  timer = setTimeout(() => { dispatch(toggleState(false)) }, 5000)
+  offTimer = setTimeout(() => { dispatch(toggleState(false)) }, 5000)
 }
 
 export const addNotification = values => dispatch => {
@@ -27,12 +27,14 @@ export const addNotification = values => dispatch => {
     time: values.time ? values.time : new Date()
   }
   dispatch(({ type: ADD_NOTIFICATION, payload: notification }))
-  stopStateOffTimer()
+  if (values.type === 'error') {
+    setTimeout(() => { dispatch(removeNotification(notification.id)) }, 4900)
+  }
   dispatch(toggleState(true))
   stateOffTimer(dispatch)
 }
 
-export const removeNotification = index => dispatch => {
+export const removeNotification = id => dispatch => {
   stopStateOffTimer()
-  dispatch({ type: REMOVE_NOTIFICATION, payload: index })
+  dispatch({ type: REMOVE_NOTIFICATION, payload: id })
 }
