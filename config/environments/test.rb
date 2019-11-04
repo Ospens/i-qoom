6fdc8b5ccf54062458f78ce6501d4325dd58b066
Kirill Shamardin <shamardin.k@gmail.com>
2019-11-04 20:26:17 +0700

    remove logo function was added

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,4 +45,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  routes.default_url_options = { host: 'lvh.me:3000' }
 end
