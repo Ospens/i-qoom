41357e65118a097cbcf6fe7472e390d37147be95
Ospens <1ospens1@gmail.com>
2019-11-19 11:16:47 +0300

    Open native file

---
+++ app/javascript/components/dashboard/dms/user/index/OpenDocument.jsx
@@ -0,0 +1,25 @@
+import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { downloadNativeFile } from '../../../../../actions/documentsActions'
+
+function OpenDocument({ docId }) {
+  const dispatch = useDispatch()
+  const openFile = useCallback(() => {
+    dispatch(downloadNativeFile(docId, open))
+  }, [dispatch, docId])
+
+  return (
+    <div>
+      <button type='button' onClick={() => openFile()}>
+        <span className='icon-common-file-text_big'>
+          <span className='path1' />
+          <span className='path2' />
+          <span className='path3' />
+          <span className='path4' />
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default OpenDocument
