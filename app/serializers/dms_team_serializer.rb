bbc1a3049fe14e49f33dfac9ba7273ac24da81e5
1v <forwardin+75785742@yandex.ru>
2019-11-22 12:43:48 +0100

    Add default rights to dms teams

--- app/serializers/dms_team_serializer.rb
+++ app/serializers/dms_team_serializer.rb
@@ -1,9 +1,26 @@
 class DmsTeamSerializer < ActiveModel::Serializer
   attributes :id,
              :name,
-             :users
+             :users,
+             :document_rights,
+             :fields
 
   def users
     object.users.as_json(only: [:id, :first_name, :last_name, :username])
+  end
+
+  def document_rights
+    object.document_rights.as_json(only: [:id,
+                                          :document_field_id,
+                                          :document_field_value_id,
+                                          :enabled,
+                                          :view_only])
+  end
+
+  def fields
+    object.convention_fields.map do |field|
+      values = DocumentFieldValue.where(document_field: field).as_json(only: [:id, :value])
+      { field.codification_kind => { id: field.id, values: values } }
+    end
   end
 end
