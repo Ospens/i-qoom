25f3fe1181d33b5fad4370b16667f4a37af32cfc
Kirill Shamardin <shamardin.k@gmail.com>
2020-01-04 00:59:43 +0700

    module access was added to session response

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,24 @@
 class UserSerializer < ApplicationSerializer
-  attributes :first_name, :last_name, :username, :email, :member_id
+  attributes :first_name,
+             :last_name,
+             :username,
+             :email,
+             :member_id,
+             :module_access
+
+  def module_access
+    projects = object.member_projects.creation_step_done
+    { cms_module_access:
+        projects.where(project_members: { cms_module_access: true })
+                .ids,
+      dms_module_access:
+        projects.where(project_members: { dms_module_access: true })
+                .ids,
+      cms_module_master: 
+        projects.where(project_members: { cms_module_master: true })
+                .ids,
+      dms_module_master: 
+        projects.where(project_members: { dms_module_master: true })
+                .ids }
+  end
 end
