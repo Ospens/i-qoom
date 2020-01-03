324d89eae05d0909182916bd755c8d6fca8dd25f
Ospens' Eye <1ospens1@gmail.com>
2020-01-03 14:02:18 +0300

    Testing

--- app/javascript/components/dashboard/dms/user/documentForm/UploadFile.jsx
+++ app/javascript/components/dashboard/dms/user/documentForm/UploadFile.jsx
@@ -5,7 +5,9 @@ import DropZoneField from '../../../../../elements/DropZoneField'
 import InputField from '../../../../../elements/InputField'
 import { required } from '../../../../../elements/validations'
 
-export const changeFile = value => (value || typeof value === 'number' ? undefined : 'Please change the file')
+export const changeFile = value => (value || typeof value === 'number'
+  ? undefined
+  : 'Please change the file')
 
 const selector = formValueSelector('document_form')
 
@@ -17,43 +19,43 @@ function UploadFile() {
   const docIndex = documentFields.findIndex(el => el.codification_kind === 'document_native_file')
 
   return (
-    <div >
-      <div className='dms-content__header'>
-        <div className='d-flex'>
+    <div>
+      <div className="dms-content__header">
+        <div className="d-flex">
           <h4>Add new revision</h4>
         </div>
       </div>
 
-      <div className='content-body bottom-padding'>
-        <div className='form-group col-6 pl-0'>
+      <div className="content-body bottom-padding">
+        <div className="form-group col-6 pl-0">
           <Field
             component={InputField}
-            name='contractor'
-            id='contractor'
-            placeholder='Originator'
-            label='Type in originator*'
+            name="contractor"
+            id="contractor"
+            placeholder="Originator"
+            label="Type in originator*"
             validate={[required]}
           />
         </div>
-        <div className='row mt-5'>
-          <div className='col-6'>
+        <div className="row mt-5">
+          <div className="col-6">
             <Field
               label={docFile.title}
-              name={`document_fields[${docIndex}].value`}
-              id={`document_fields[${docIndex}].value`}
+              name={`document_fields[${docIndex}].file`}
+              id={`document_fields[${docIndex}].file`}
               validate={[changeFile]}
               placeholder={docFile.command}
               component={DropZoneField}
-              renameFile={true}
+              renameFile
               filename={docFile.filename}
             />
           </div>
         </div>
       </div>
 
-      <div className='dms-footer'>
-        <button type='button' className='btn btn-white'>Cancel</button>
-        <button type='submit' className='btn btn-purple' >
+      <div className="dms-footer">
+        <button type="button" className="btn btn-white">Cancel</button>
+        <button type="submit" className="btn btn-purple">
           Next
         </button>
       </div>
