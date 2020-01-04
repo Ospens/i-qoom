3977b6e13bd9a9bfa8c1f2f66311ac91cd7e2be9
Kirill Shamardin <shamardin.k@gmail.com>
2020-01-04 21:59:17 +0700

    session user serializer was added and module_access data was moved to it from user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,22 +3,5 @@ class UserSerializer < ApplicationSerializer
              :last_name,
              :username,
              :email,
-             :member_id,
-             :module_access
-
-  def module_access
-    projects = object.member_projects.creation_step_done
-    { cms_module_access:
-        projects.where(project_members: { cms_module_access: true })
-                .ids,
-      dms_module_access:
-        projects.where(project_members: { dms_module_access: true })
-                .ids,
-      cms_module_master: 
-        projects.where(project_members: { cms_module_master: true })
-                .ids,
-      dms_module_master: 
-        projects.where(project_members: { dms_module_master: true })
-                .ids }
-  end
+             :member_id
 end
